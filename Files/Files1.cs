b06754315c180cb51d9ea4e6f2701108ddf968ac
vaclja <rewitablexmetin@gmail.com>
2025-07-10 20:46:13 +0200

    Přidejte soubory projektu.

---
+++ Files/Files1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Files;
+
+internal class Files1
+{
+    public void Delete()
+    {
+        string path = @"C:\Users\rewit\Downloads\File";
+        DirectoryInfo dir = new DirectoryInfo(path);
+        if (dir.Exists)
+        {
+            FileInfo[] files = dir.GetFiles("*", SearchOption.AllDirectories);
+            foreach (FileInfo file in files)
+            {
+                Console.WriteLine($"Název souboru {file.FullName}");
+               
+            }
+            Console.WriteLine("Který soubor se má smazat?");
+            string fileName = Console.ReadLine();
+            if (fileName != null)
+            {
+                File.Delete(fileName);
+                Console.WriteLine($"Soubor {fileName} byl smazán.");
+            }
+            else
+            {
+                Console.WriteLine("Soubor nebyl nalezen.");
+            }
+        }
+    }
+    public static void Mainx(string[] args)
+    {
+        Files1 file = new Files1();
+        file.Delete();
+    }
+}
---
+++ Files/Program.cs
@@ -0,0 +1,17 @@
+﻿using Files;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Files1.Mainx(args);
+        }
+    }
+}
